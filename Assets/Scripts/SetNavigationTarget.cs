250835d55b1efbf357e1ae7bf45dda06dfedb95a
konndia <nadiya-khomenko@mail.ru>
2023-11-08 11:27:38 +0500

    First Floor (Test)
    Last details from 1st video, need to be tested in the building

---
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetNavigationTarget : MonoBehaviour
+{
+    [SerializeField]
+    private Camera topDownCamera;
+    [SerializeField]
+    private GameObject navTargetObject;
+
+    private NavMeshPath path;
+    private LineRenderer line;
+
+    private bool lineToggle = false;
+
+    private void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            lineToggle = !lineToggle;
+        }
+
+        if (lineToggle)
+        {
+            NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            line.SetPositions(path.corners);
+            line.enabled = true;
+        }
+    }
+}
